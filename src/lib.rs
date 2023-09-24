e252aec23878254537564e65b3160ee578bc79c4
Peter Krull <peterkrullpeter@gmail.com>
2023-09-24 18:57:11 +0200

    Use Float from num-traits instead of hardcoded f32. Renamed circular -> wrapping. Now wrapping error value input

--- src/lib.rs
+++ src/lib.rs
@@ -1,221 +1,266 @@
 #![feature(type_changing_struct_update)]
 #![no_std]
 
-pub struct NoCircular;
-pub struct Circular {
-    pub min: f32,
-    pub max: f32,
+use core::ops::{AddAssign, SubAssign};
+
+use num_traits::float::Float;
+
+pub struct NoWrapping;
+pub struct Wrapping<F> {
+    pub min: F,
+    pub max: F,
 }
 
 pub struct NoOutputLimit;
-pub struct OutputLimit {
-    pub min: f32,
-    pub max: f32,
+pub struct OutputLimit<F> {
+    pub min: F,
+    pub max: F,
 }
 
 pub struct NoAntiWindup;
-pub enum AntiWindup {
-    Conditional((f32, f32)),
+pub enum AntiWindup<F> {
+    Conditional((F, F)),
 }
 
 pub struct NoLpFilter;
-pub struct LpFilter {
-    tau : f32,
-    signal : f32,
+pub struct LpFilter<F> {
+    tau: F,
+    signal: F,
 }
 
-pub struct Pid<CIRC,LIMIT,WINDUP,LPFILT> {
-    kp: f32,
-    ki: f32,
-    kd: f32,
-    ts: f32,
-    integral: f32,
+pub struct Pid<F, WRAP, LIMIT, WINDUP, LPFILT> {
+    kp: F,
+    ki: F,
+    kd: F,
+    ts: F,
+    integral: F,
     integral_en: bool,
-    prev_error: f32,
-    circular: CIRC,
+    prev_error: F,
+    wrapping: WRAP,
     output_limit: LIMIT,
     anti_windup: WINDUP,
-    lp_filter : LPFILT,
+    lp_filter: LPFILT,
 }
 
-impl Pid<NoCircular,NoOutputLimit,NoAntiWindup,NoLpFilter> {
-    pub fn new(kp:f32, ki:f32, kd:f32, ideal:bool, ts:f32) -> Pid<NoCircular,NoOutputLimit,NoAntiWindup,NoLpFilter> {
+impl<F: Float + Default> Pid<F, NoWrapping, NoOutputLimit, NoAntiWindup, NoLpFilter> {
+    pub fn new(
+        kp: F,
+        ki: F,
+        kd: F,
+        ideal: bool,
+        ts: F,
+    ) -> Pid<F, NoWrapping, NoOutputLimit, NoAntiWindup, NoLpFilter> {
         Pid {
             kp,
-            ki : if ideal {ki*kp} else {ki},
-            kd : if ideal {kd*kp} else {kd} ,
+            ki: if ideal { ki * kp } else { ki },
+            kd: if ideal { kd * kp } else { kd },
             ts,
-            integral : 0.,
-            integral_en : true,
-            prev_error : 0.,
-            circular : NoCircular,
-            output_limit : NoOutputLimit,
-            anti_windup : NoAntiWindup,
-            lp_filter : NoLpFilter
+            integral: Default::default(),
+            integral_en: true,
+            prev_error: Default::default(),
+            wrapping: NoWrapping,
+            output_limit: NoOutputLimit,
+            anti_windup: NoAntiWindup,
+            lp_filter: NoLpFilter,
         }
     }
 }
 
-impl<ANYCIRC,ANYLIMIT,ANYWINDUP,ANYFILT> Pid<ANYCIRC,ANYLIMIT,ANYWINDUP,ANYFILT> {
-
-
-    pub fn set_circular(self, min: f32, max : f32) -> Pid<Circular,ANYLIMIT,ANYWINDUP,ANYFILT> {
-        Pid{ circular : Circular { min, max } , ..self }
+impl<F: Float + Default, ANYWRAP, ANYLIMIT, ANYWINDUP, ANYFILT>
+    Pid<F, ANYWRAP, ANYLIMIT, ANYWINDUP, ANYFILT>
+{
+    pub fn set_wrapping(self, min: F, max: F) -> Pid<F, Wrapping<F>, ANYLIMIT, ANYWINDUP, ANYFILT> {
+        Pid {
+            wrapping: Wrapping { min, max },
+            ..self
+        }
     }
 
-    pub fn set_output_limit(self, output_limit: OutputLimit) -> Pid<ANYCIRC,OutputLimit,ANYWINDUP,ANYFILT> {
-        Pid{ output_limit , ..self }
+    pub fn set_output_limit(
+        self,
+        output_limit: OutputLimit<F>,
+    ) -> Pid<F, ANYWRAP, OutputLimit<F>, ANYWINDUP, ANYFILT> {
+        Pid {
+            output_limit,
+            ..self
+        }
     }
 
-    pub fn set_anti_windup(self, anti_windup: AntiWindup) -> Pid<ANYCIRC,ANYLIMIT,AntiWindup,ANYFILT> {
-        Pid{ anti_windup , ..self }
+    pub fn set_anti_windup(
+        self,
+        anti_windup: AntiWindup<F>,
+    ) -> Pid<F, ANYWRAP, ANYLIMIT, AntiWindup<F>, ANYFILT> {
+        Pid {
+            anti_windup,
+            ..self
+        }
     }
 
-    pub fn set_lp_filter(self, tau: f32) -> Pid<ANYCIRC,ANYLIMIT,ANYWINDUP,LpFilter> {
-        Pid{ lp_filter : LpFilter{ tau, signal : 0. } , ..self }
+    pub fn set_lp_filter(self, tau: F) -> Pid<F, ANYWRAP, ANYLIMIT, ANYWINDUP, LpFilter<F>> {
+        Pid {
+            lp_filter: LpFilter {
+                tau,
+                signal: Default::default(),
+            },
+            ..self
+        }
     }
 
-    pub fn set_gains(&mut self, kp: f32, ki: f32, kd: f32) {
+    pub fn set_gains(&mut self, kp: F, ki: F, kd: F) {
         self.kp = kp;
         self.ki = ki;
         self.kd = kd;
     }
 
-    pub fn reset_integral(& mut self) {
-        self.integral = 0.0;
+    pub fn reset_integral(&mut self) {
+        self.integral = Default::default();
     }
 
-    pub fn reset_integral_to(& mut self, integral : f32) {
+    pub fn reset_integral_to(&mut self, integral: F) {
         self.integral = integral;
     }
 
-    pub fn enable_integral(& mut self, enable : bool) {
+    pub fn enable_integral(&mut self, enable: bool) {
         self.integral_en = enable;
     }
 }
 
-impl <ANYCIRC,ANYLIMIT,ANYWINDUP,ANYFILT> Pid<ANYCIRC,ANYLIMIT,ANYWINDUP,ANYFILT>
+impl<F: Float, ANYWRAP, ANYLIMIT, ANYWINDUP, ANYFILT> Pid<F, ANYWRAP, ANYLIMIT, ANYWINDUP, ANYFILT>
 where
-    Pid<ANYCIRC,ANYLIMIT,ANYWINDUP,ANYFILT> : CircularTrait + OutputLimitTrait + AntiWindupTrait + LpFilterTrait
+    Pid<F, ANYWRAP, ANYLIMIT, ANYWINDUP, ANYFILT>:
+        WrappingTrait<F> + OutputLimitTrait<F> + AntiWindupTrait<F> + LpFilterTrait<F>,
 {
-
-    pub fn update_ts(&mut self, error: f32, ts: f32) -> f32 {
-
+    pub fn update_ts(&mut self, mut error: F, ts: F) -> F {
         // Constrain input
-        self.apply_circular(&error);
+        self.apply_wrapping(&mut error);
 
         // Proportional gain
         let proportional = self.kp * error;
 
         // Derivative gain
-        let mut derivative =  self.kd * (error - self.prev_error) / ts;
-        self.apply_lp_filter(& mut derivative, &ts);
+        let mut derivative = self.kd * (error - self.prev_error) / ts;
+        self.apply_lp_filter(&mut derivative, ts);
         self.prev_error = error;
 
         // Integral gain
         if self.integral_en {
-            self.apply_anti_windup( self.ki * error * ts , proportional + derivative);
+            self.apply_anti_windup(self.ki * error * ts, proportional + derivative);
         }
 
         // Constrain output
         self.apply_output_limit(proportional + self.integral + derivative)
-
     }
 
-    pub fn update(&mut self, error: f32) -> f32 {
-
-       self.update_ts(error, self.ts)
-
+    pub fn update(&mut self, error: F) -> F {
+        self.update_ts(error, self.ts)
     }
 }
 
 // Wrapping of input
-pub trait CircularTrait {
-    fn apply_circular(&mut self, input: &f32);
+pub trait WrappingTrait<F> {
+    fn apply_wrapping(&mut self, input: &mut F);
 }
 
-impl<ANYLIMIT,ANYWINDUP,ANYFILT> CircularTrait for Pid<Circular,ANYLIMIT,ANYWINDUP,ANYFILT> {
+impl<F: Float + AddAssign + SubAssign, ANYLIMIT, ANYWINDUP, ANYFILT> WrappingTrait<F>
+    for Pid<F, Wrapping<F>, ANYLIMIT, ANYWINDUP, ANYFILT>
+{
     #[inline(always)]
-    fn apply_circular(&mut self, error: &f32) {
-        if error - self.prev_error < self.circular.min {
-            self.prev_error -= self.circular.max - self.circular.min;
-            self.integral -= self.circular.max - self.circular.min;
-        } else if error - self.prev_error > self.circular.max {
-            self.prev_error += self.circular.max - self.circular.min;
-            self.integral += self.circular.max - self.circular.min;
+    fn apply_wrapping(&mut self, error: &mut F) {
+        let minmaxdiff = self.wrapping.max - self.wrapping.min;
+
+        // Wrap the error itself (aggressively)
+        let frac = ((*error-self.wrapping.min)/(self.wrapping.max-self.wrapping.min)).floor();
+        *error -= frac*(self.wrapping.max-self.wrapping.min);
+
+        // Wrap previous error and integral
+        if *error - self.prev_error < self.wrapping.min {
+            self.prev_error -= minmaxdiff;
+            self.integral -= minmaxdiff;
+        } else if *error - self.prev_error > self.wrapping.max {
+            self.prev_error += minmaxdiff;
+            self.integral += minmaxdiff;
         }
     }
 }
 
-impl<ANYLIMIT,ANYWINDUP,ANYFILT> CircularTrait for Pid<NoCircular,ANYLIMIT,ANYWINDUP,ANYFILT> {
-
+impl<F: Float, ANYLIMIT, ANYWINDUP, ANYFILT> WrappingTrait<F>
+    for Pid<F, NoWrapping, ANYLIMIT, ANYWINDUP, ANYFILT>
+{
     #[inline(always)]
-    fn apply_circular(&mut self, _input: &f32) {}
+    fn apply_wrapping(&mut self, _input: &mut F) {}
 }
 
 // Constraining of output
-pub trait OutputLimitTrait {
-    fn apply_output_limit(&mut self, input: f32 ) -> f32;
+pub trait OutputLimitTrait<F> {
+    fn apply_output_limit(&mut self, input: F) -> F;
 }
 
-impl<ANYCIRC,ANYWINDUP,ANYFILT> OutputLimitTrait for Pid<ANYCIRC,OutputLimit,ANYWINDUP,ANYFILT> {
+impl<F: Float + Ord, ANYWRAP, ANYWINDUP, ANYFILT> OutputLimitTrait<F>
+    for Pid<F, ANYWRAP, OutputLimit<F>, ANYWINDUP, ANYFILT>
+{
     #[inline(always)]
-    fn apply_output_limit(&mut self, input: f32 ) -> f32 {
+    fn apply_output_limit(&mut self, input: F) -> F {
         input.clamp(self.output_limit.min, self.output_limit.max)
     }
 }
 
-impl<ANYCIRC,ANYWINDUP,ANYFILT> OutputLimitTrait for Pid<ANYCIRC,NoOutputLimit,ANYWINDUP,ANYFILT> {
-
+impl<F: Float, ANYWRAP, ANYWINDUP, ANYFILT> OutputLimitTrait<F>
+    for Pid<F, ANYWRAP, NoOutputLimit, ANYWINDUP, ANYFILT>
+{
     #[inline(always)]
-    fn apply_output_limit(&mut self, input: f32  ) -> f32 {
+    fn apply_output_limit(&mut self, input: F) -> F {
         input
     }
 }
 
 // Anti-windup for integral
-pub trait AntiWindupTrait {
-    fn apply_anti_windup(&mut self, input: f32, pd : f32 );
+pub trait AntiWindupTrait<F> {
+    fn apply_anti_windup(&mut self, input: F, pd: F);
 }
 
-impl<ANYCIRC,ANYLIMIT,ANYFILT> AntiWindupTrait for Pid<ANYCIRC,ANYLIMIT,AntiWindup,ANYFILT> {
+impl<F: Float + AddAssign, ANYWRAP, ANYLIMIT, ANYFILT> AntiWindupTrait<F>
+    for Pid<F, ANYWRAP, ANYLIMIT, AntiWindup<F>, ANYFILT>
+{
     #[inline(always)]
-    fn apply_anti_windup(&mut self, input: f32 , pd : f32 ) {
+    fn apply_anti_windup(&mut self, input: F, pd: F) {
         match self.anti_windup {
-            AntiWindup::Conditional((min,max)) => {
+            AntiWindup::Conditional((min, max)) => {
                 if pd > min && pd < max {
                     self.integral += input;
                 }
-            },
+            }
         }
     }
 }
 
-impl<ANYCIRC,ANYLIMIT,ANYFILT> AntiWindupTrait for Pid<ANYCIRC,ANYLIMIT,NoAntiWindup,ANYFILT> {
-
+impl<F: Float, ANYWRAP, ANYLIMIT, ANYFILT> AntiWindupTrait<F>
+    for Pid<F, ANYWRAP, ANYLIMIT, NoAntiWindup, ANYFILT>
+{
     #[inline(always)]
-    fn apply_anti_windup(&mut self, input: f32 , _pd : f32 ) {
-        self.integral += input;
+    fn apply_anti_windup(&mut self, input: F, _pd: F) {
+        self.integral = self.integral + input;
     }
 }
 
 // Low pass filter for derivative
-pub trait LpFilterTrait {
-    fn apply_lp_filter(&mut self, input: &mut f32, ts: &f32);
+pub trait LpFilterTrait<F> {
+    fn apply_lp_filter(&mut self, input: &mut F, ts: F);
 }
 
-impl<ANYCIRC,ANYLIMIT,ANYWINDUP> LpFilterTrait for Pid<ANYCIRC,ANYLIMIT,ANYWINDUP,LpFilter> {
+impl<F: Float, ANYWRAP, ANYLIMIT, ANYWINDUP> LpFilterTrait<F>
+    for Pid<F, ANYWRAP, ANYLIMIT, ANYWINDUP, LpFilter<F>>
+{
     #[inline(always)]
-    fn apply_lp_filter(&mut self, input: &mut f32, ts: &f32) {
-
-        let a = ts/(ts+self.lp_filter.tau);
-        self.lp_filter.signal = self.lp_filter.signal*(1.0-a) + *input*a;
+    fn apply_lp_filter(&mut self, input: &mut F, ts: F) {
+        let a = ts / (ts + self.lp_filter.tau);
+        self.lp_filter.signal = self.lp_filter.signal * (F::one() - a) + *input * a;
         *input = self.lp_filter.signal;
     }
 }
 
-impl<ANYCIRC,ANYLIMIT,ANYWINDUP> LpFilterTrait for Pid<ANYCIRC,ANYLIMIT,ANYWINDUP,NoLpFilter> {
-
+impl<F: Float, ANYWRAP, ANYLIMIT, ANYWINDUP> LpFilterTrait<F>
+    for Pid<F, ANYWRAP, ANYLIMIT, ANYWINDUP, NoLpFilter>
+{
     #[inline(always)]
-    fn apply_lp_filter(&mut self, _input: &mut f32, _ts: &f32) {}
+    fn apply_lp_filter(&mut self, _input: &mut F, _ts: F) {}
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -6,8 +6,7 @@ mod tests {
 
     #[test]
     fn test_proportional() {
-
-        let mut controller = Pid::new(2.5, 0.0, 0.0, false,0.25);
+        let mut controller = Pid::new(2.5f32, 0.0, 0.0, false, 0.25);
 
         let output = controller.update_ts(3.0, 0.25);
         assert_approx_eq!(output, 7.5, 1e-6);
@@ -15,56 +14,49 @@ mod tests {
 
     #[test]
     fn test_derivative() {
-
-        let mut controller = Pid::new(2.5, 0.0, 0.5, false,0.25);
+        let mut controller = Pid::new(2.5f32, 0.0, 0.5, false, 0.25);
 
         let mut output = controller.update_ts(3.0, 0.25);
-        assert_approx_eq!(output, 7.5 + 3.0*0.5/0.25, 1e-6);
+        assert_approx_eq!(output, 7.5 + 3.0 * 0.5 / 0.25, 1e-6);
 
         output = controller.update_ts(3.0, 0.25);
         assert_approx_eq!(output, 7.5, 1e-6);
 
         output = controller.update_ts(0.0, 0.25);
-        assert_approx_eq!(output, - 3.0*0.5/0.25, 1e-6);
-
+        assert_approx_eq!(output, -3.0 * 0.5 / 0.25, 1e-6);
     }
 
     #[test]
     fn test_integral() {
-
-        let mut controller = Pid::new(2.5, 0.2, 0.0, false,0.25);
+        let mut controller = Pid::new(2.5f32, 0.2, 0.0, false, 0.25);
 
         let mut output = controller.update_ts(3.0, 0.25);
-        assert_approx_eq!(output, 7.5 + 3.0*0.2*0.25, 1e-6);
+        assert_approx_eq!(output, 7.5 + 3.0 * 0.2 * 0.25, 1e-6);
 
         output = controller.update_ts(3.0, 0.25);
-        assert_approx_eq!(output, 7.5 + 6.0*0.2*0.25, 1e-6);
+        assert_approx_eq!(output, 7.5 + 6.0 * 0.2 * 0.25, 1e-6);
 
         output = controller.update_ts(0.0, 0.25);
-        assert_approx_eq!(output, 6.0*0.2*0.25, 1e-6);
-
+        assert_approx_eq!(output, 6.0 * 0.2 * 0.25, 1e-6);
     }
 
     #[test]
     fn test_integral_ideal() {
-
-        let mut controller = Pid::new(2.5, 0.2, 0.0, true,0.25);
+        let mut controller = Pid::new(2.5f32, 0.2, 0.0, true, 0.25);
 
         let mut output = controller.update_ts(3.0, 0.25);
-        assert_approx_eq!(output, 7.5 + 2.5*3.0*0.2*0.25, 1e-6);
+        assert_approx_eq!(output, 7.5 + 2.5 * 3.0 * 0.2 * 0.25, 1e-6);
 
         output = controller.update_ts(3.0, 0.25);
-        assert_approx_eq!(output, 7.5 + 2.5*6.0*0.2*0.25, 1e-6);
+        assert_approx_eq!(output, 7.5 + 2.5 * 6.0 * 0.2 * 0.25, 1e-6);
 
         output = controller.update_ts(0.0, 0.25);
-        assert_approx_eq!(output, 2.5*6.0*0.2*0.25, 1e-6);
-
+        assert_approx_eq!(output, 2.5 * 6.0 * 0.2 * 0.25, 1e-6);
     }
 
     #[test]
     fn test_integral_ideal_0kp() {
-
-        let mut controller = Pid::new(0.0, 0.2, 5.0, true,0.25);
+        let mut controller = Pid::new(0.0f32, 0.2, 5.0, true, 0.25);
 
         let mut output = controller.update_ts(3.0, 0.25);
         assert_approx_eq!(output, 0.0, 1e-6);
@@ -74,6 +66,5 @@ mod tests {
 
         output = controller.update_ts(0.0, 0.25);
         assert_approx_eq!(output, 0.0, 1e-6);
-
     }
 }
